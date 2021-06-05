771deb58ba849c81363c26c5a43218d0a1cdd806
Henry Saint Juste <hbstjuste@gmail.com>
2021-06-05 15:25:15 -0700

    connect home component with backend via http protocol

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,10 +1,11 @@
-const axios = require("axios");
+const axios = require('axios')
 
 const http = {
   post: axios.post,
   get: axios.get,
   put: axios.put,
   delete: axios.delete,
-};
+  url: 'https://weather-app-caching.herokuapp.com/api/weather?lat=33.44&lon=-112.07',
+}
 
-export default http;
+export default http
