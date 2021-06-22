f809b5383efac267d0771a23bd4bd514385b850e
Henry Saint Juste <hbstjuste@gmail.com>
2021-06-22 15:25:21 -0700

    update dateTime and day functions

--- src/functions/dateTime.js
+++ src/functions/dateTime.js
@@ -1,10 +1,16 @@
 const date = (unix) => {
-  return new Date(unix * 1000).toLocaleDateString("en-US");
-};
+  return new Date(unix * 1000).toLocaleDateString('en-US')
+}
 
 const time = (unix) => {
-  return new Date(unix * 1000).toLocaleTimeString("en-US");
-};
+  return (
+    new Date(unix * 1000)
+      .toLocaleTimeString('en-US')
+      .split(' ')[0]
+      .split(':')[0] +
+    new Date(unix * 1000).toLocaleTimeString('en-US').split(' ')[1]
+  )
+}
 
-module.exports.time = time;
-module.exports.formateDate = date;
+module.exports.time = time
+module.exports.formateDate = date
---
+++ src/functions/day.js
@@ -0,0 +1,14 @@
+const day = (int) => {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  return days[int]
+}
+
+module.exports.day = day
